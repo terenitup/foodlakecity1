68e46cd5b7b33dc65a205e9f13da2e44f3505aff
terenitup <terentaniuchi@gmail.com>
2018-09-21 13:12:06 -0600

    updated project title

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>FoodLakeCity</h1>
+        <h1>Discover Great Eats in SLC and beyond with</h1>
       </div>
     );
   }
